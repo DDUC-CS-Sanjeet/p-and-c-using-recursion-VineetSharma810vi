df7cdf1acb9348818b7201441dde495eec17c420
VineetSharma810vi <59726956+VineetSharma810vi@users.noreply.github.com>
2020-01-24 17:10:28 +0530

    vineet sharma permutation via recursion

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -3,15 +3,63 @@ using namespace std;
 
 int permutation(int n, int r)
 {
+	if(r==0)
+	{
+		return 1;
+	}
+	else
+	{
+	   return (n)*permutation(n-1,r-1);	
+	}
   return 0;
 }
 
 int combination(int n, int r)
 {
+	if(r==0)
+	{
+		return 1;
+	}
+	else
+	{
+	
+	return (n/r)*combination(n-1,r-1);
+}
   return 0;
 }
 
 int main()
 {
+		int x=1;
+	float p,c,n,r;
+	cout<<"Enter values of n and r:";
+	cin>>n>>r;
+	trymklink
+	{
+		if(r==0)
+		{
+			p=1;
+			c=1;
+		}
+		else if(n<r)
+		{
+			throw x;
+		}
+		else if(n<0||r<0)
+		{
+			throw x;
+		}
+		else 
+		{
+			p=permutation(n,r);
+			c=combination(n,r);
+		}
+		cout<<"\n permutation = "<<p;
+		cout<<"\n combination = "<<c;
+	}
+	catch(int a)
+	{
+		cout<<"\n invalid ";
+	}
   return 0;
 }
